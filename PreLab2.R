9418c4b2f4359331aa1f98570dc07ffd463c3d93
Carlos Uziel Jiménez Toledo <chuck-salle@hotmail.com>
2016-02-24 17:41:54 -0600

    Create PreLab2.R

---
+++ PreLab2.R
@@ -0,0 +1,25 @@
+#-------------------------------------Pre Lab 2----------------------------------------------#
+
+#Primary Research Question
+#How many days do animals spend in the shelter before they are adopted?
+
+#Load the data
+
+library(SDSFoundations)
+animaldata = AnimalData
+
+#Find the number of animals that were adopted
+table(animaldata$Outcome.Type)
+
+#Pull out only adopted animals
+adopted <- animaldata[animaldata$Outcome.Type=="Adoption",]
+
+#Pull out just the days in shelter for the adopted animals
+daystoadopt <- adopted$Days.Shelter
+
+#Visualize and describe this variable
+hist(daystoadopt)
+fivenum(daystoadopt)
+mean(daystoadopt)
+sd(daystoadopt)
+which(animaldata$Days.Shelter==max(daystoadopt))
